c7bf1b3e741ad4cdd16fd70e73d97fab26a2ed2d
Megan Sullivan <meganesu@usc.edu>
2015-09-17 21:18:43 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+  	@user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params) # "strong parameters" technique (see private method)
+  	if @user.save
+  		flash[:success] = "Welcome to Odyssey! Click 'New Trip' to get started."
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'help' => 'static_pages#help'
   get 'signup' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
